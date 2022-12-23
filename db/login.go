ca66fc06358e1a0d7797be34bb25699c014b754d
strongwill <92038690+dezhoutorizhao@users.noreply.github.com>
2022-12-23 22:40:43 +0800

    Add files via upload

---
+++ db/login.go
@@ -0,0 +1,117 @@
+package db
+
+import (
+	_ "context"
+	_ "database/sql"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+	Id int
+	UserName string
+	Password string
+	Email string
+}
+
+
+var (
+	id int
+	username string
+	password string
+	email string
+	login_status int
+)
+
+
+func (user *User) GetUserByUsername(un string) (*User,error) {
+	// sql语句
+	sqlStr := "SELECT id, username, password, email FROM user_login.users WHERE username = ?";
+	// QueryRow执行一次查询，并期望返回最多一行结果，即row
+	fmt.Println(sqlStr)
+	if Db == nil {
+		return nil, fmt.Errorf("Db is nil")
+	}
+	rows, err := Db.Query(sqlStr, un)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &username, &password, &email)
+		if err != nil {
+			return nil, err
+		}
+		u := &User{
+			Id: id,
+			UserName: username,
+			Password: password,
+			Email: email,
+		}
+		fmt.Println(u)
+		return u, nil
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+
+func checkLogin(username string, password string) int {
+	// user是从前端接收的数据
+	user := &User{}
+	// u是根据username查询到的数据
+	u, err := user.GetUserByUsername(username)
+	fmt.Println(username,password)
+	fmt.Println(u)
+	if err != nil {
+		fmt.Println(err)
+		login_status = 0
+		return 500 // Internal Server Error
+	}
+	if u == nil {
+		fmt.Println("404")
+		login_status = 0
+		return 404 // Not Found
+	}
+	if u.Password == password {
+		fmt.Println("login successfully")
+		//log_status := If_Success{"yes"}
+		//c.JSON(200,log_status)
+		login_status = 200
+		return 200 // OK
+	} else {
+		fmt.Println("401")
+		login_status = 0
+		return 401 // Unauthorized
+	}
+}
+
+func Login(c *gin.Context) {
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	statusCode := checkLogin(username, password)
+
+	fmt.Println(statusCode)
+
+	if statusCode == 200 {
+		// Login successful, redirect to dashboard or set cookies
+	} else if statusCode == 401 {
+		// Invalid credentials, display error message
+	} else {
+		// Other error, display appropriate message
+	}
+
+}
+
+func If_success(c *gin.Context) {
+	if login_status == 200 {
+		c.String(200,"success")
+	}
+}
---
+++ db/register.go
@@ -0,0 +1,55 @@
+package db
+
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	re_username string
+	re_password string
+	re_email string
+	re_number string
+)
+
+// re代表register
+type Re_User struct {
+	re_username string
+	re_password string
+	re_email string
+	re_number string
+}
+
+// 接收从前端返回过来的数据部分
+func AddUser_front(add_c *gin.Context) {
+	re_username = add_c.PostForm("username")
+	re_password = add_c.PostForm("password")
+	re_email = add_c.PostForm("email")
+	re_number = add_c.PostForm("number")
+	fmt.Println(re_username,re_password,re_email)
+	// re代表register
+	u := &Re_User{re_username,re_password,re_email,re_number}
+	u.AddUser()
+}
+
+// AddUser 添加用户的方法一
+func (user *Re_User) AddUser() error {
+//写sql语句
+sqlStr := "insert into register_users(username,password,email,number) values(?,?,?,?)"
+//预编译
+fmt.Println(sqlStr)
+inStmt , err := Db.Prepare(sqlStr) //预编译得到的是inStmt,通过操作inStmt得到不同的结果
+if err != nil {
+fmt.Println("预编译出现异常",err)
+return err
+}
+//3.执行
+fmt.Println(inStmt)
+_,err2 := inStmt.Exec(re_username,re_password,re_email,re_number)
+if err2 != nil {
+fmt.Println("执行出现异常",err2)
+return err2
+}
+return nil
+}
